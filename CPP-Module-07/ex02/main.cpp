e09e1915032639d4ca1caac395a6bb2e00fc2982
gjacqual <gjacqual@student.21-school.ru>
2022-08-23 15:15:43 +0300

    mod07ex02 completed and checked

--- CPP-Module-07/ex02/main.cpp
+++ CPP-Module-07/ex02/main.cpp
@@ -18,7 +18,7 @@ int main(int, char**) {
         Array<int> arrayInt;
         try {
             arrayInt[0] = 10;
-        } catch (std::exception &e) {
+        } catch (std::exception& e) {
             std::cerr << C_RED << e.what() << C_CLEAR << std::endl;
         }
 
@@ -26,16 +26,19 @@ int main(int, char**) {
     std::cout << C_GREEN << "**************************************************"
               << C_CLEAR << std::endl;
     // **************************** Test 2 ***********************************//
-    std::cout << C_GREEN << "<<<-Test1: Create Array with specific size->>>"
+    std::cout << C_GREEN << "<<<-Test2: Create Array with specific size->>>"
               << C_CLEAR << std::endl;
     {
         Array<char> arrayChar(873);
         try {
             arrayChar[836] = 'a';
-        } catch (std::exception &e) {
+            std::cout << "A member function size() test: "
+                      << C_GREEN << arrayChar.size() << C_CLEAR << std::endl;
+        } catch (std::exception& e) {
             std::cerr << C_RED << e.what() << C_CLEAR << std::endl;
         }
-        std::cout << arrayChar[836] << std::endl;
+        std::cout << "Print element by Index: '" << arrayChar[836] << "'"
+                  << C_CLEAR << std::endl;
 
     }
     std::cout << C_GREEN << "**************************************************"
@@ -46,8 +49,7 @@ int main(int, char**) {
     Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
     srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
+    for (int i = 0; i < MAX_VAL; i++) {
         const int value = rand();
         numbers[i] = value;
         mirror[i] = value;
@@ -58,36 +60,29 @@ int main(int, char**) {
         Array<int> test(tmp);
     }
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
+    for (int i = 0; i < MAX_VAL; i++) {
+        if (mirror[i] != numbers[i]) {
             std::cerr << "didn't save the same value!!" << std::endl;
             return 1;
         }
     }
-    try
-    {
+    try {
         numbers[-2] = 0;
     }
-    catch(const std::exception& e)
-    {
+    catch (const std::exception& e) {
         std::cerr << e.what() << '\n';
     }
-    try
-    {
+    try {
         numbers[MAX_VAL] = 0;
     }
-    catch(const std::exception& e)
-    {
+    catch (const std::exception& e) {
         std::cerr << e.what() << '\n';
     }
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
+    for (int i = 0; i < MAX_VAL; i++) {
         numbers[i] = rand();
     }
-    delete [] mirror;//
+    delete[] mirror;//
 
 
     return 0;
