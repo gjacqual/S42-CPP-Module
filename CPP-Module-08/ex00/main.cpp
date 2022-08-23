26630e6b9f22d174c91d7bbb8b3f79794495f49f
gjacqual <gjacqual@student.21-school.ru>
2022-08-23 17:48:49 +0300

    module08ex00 start

---
+++ CPP-Module-08/ex00/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+#define C_CLEAR     "\033[0m"
+#define C_GREEN     "\033[0;32m"
+
+int main() {
+
+    // **************************** Test 1 ***********************************//
+    std::cout << C_GREEN << "<<<-Test1: From Subject->>>"
+              << C_CLEAR << std::endl;
+    {
+
+    }
+    return 0;
+}
